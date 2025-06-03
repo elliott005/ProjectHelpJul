9a62e4e8620138fc107011c074836248db2dd0d2
elliott005 <elliott.castellazzi@gmail.com>
2025-06-03 14:55:55 +0200

    fixes + web version

--- src/bar.gd
+++ src/bar.gd
@@ -1,9 +1,11 @@
 extends ProgressBar
 
 @export var color: Color
-@export var refill_rate = 10
+@export var refill_rate = 1
 @export var increase_amount = 10
 @export var decrease_amount = 10
+@export var increase_delta_amount = 1
+@export var decrease_delta_amount = 1
 
 func _ready():
 	modulate = color
@@ -11,16 +13,18 @@ func _ready():
 func _process(delta):
 	value = min(value + refill_rate * delta, max_value)
 
-func increase_value():
-	value = min(value + increase_amount, max_value)
 
-func decrease_value():
+func _on_button_minus_pressed():
 	value = max(value - decrease_amount, min_value)
 
 
-func _on_button_minus_pressed():
-	decrease_value()
-
-
 func _on_button_plus_pressed():
-	increase_value()
+	value = min(value + increase_amount, max_value)
+
+
+func _on_button_minus_delta_pressed():
+	refill_rate -= decrease_delta_amount
+
+
+func _on_button_plus_delta_pressed():
+	refill_rate += increase_delta_amount
